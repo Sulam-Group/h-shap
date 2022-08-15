948de4612ca3deb8c5d349fb8e8a3d684c8239f1
Jacopo Teneggi <jtenegg1@jh.edu>
2022-08-15 19:09:54 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 PACKAGE_NAME = "h-shap"
 DESCRIPTION = "Fast Hierarchical Games for Image Explanations"
 LONG_DESCRIPTION = "`h-shap` provides a fast, hierarchical implementation of Shapley coefficients for image explanations. It is exact, and it does not rely on approximation. In binary classification scenarios, `h-shap` guarantees an exponential computational advantage when explaining an important concept contained in the image."
