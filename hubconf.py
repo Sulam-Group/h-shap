ab4017d8cbd0345ab8a924da1f58fdbc2e18ddc4
Jacopo Teneggi <jtenegg1@jh.edu>
2022-08-21 22:47:21 -0400

    Add hubconf.py and weights

---
+++ hubconf.py
@@ -0,0 +1,16 @@
+dependencies = ["torch"]
+
+import os
+import torch
+from demo.RSNA_ICH_detection.model import RSNAHemorrhageNet as _rsnahemorrhagenet
+
+
+def rsnahemorrhagenet(pretrained=False, **kwargs):
+    model = _rsnahemorrhagenet()
+    if pretrained:
+        dirname = os.path.dirname(__file__)
+        state_dict = torch.load(
+            os.path.join(dirname, "demo", "RSNA_ICH_detection", "model.pt")
+        )
+        model.load_state_dict(state_dict)
+    return model
