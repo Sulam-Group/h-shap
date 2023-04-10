a458b8660c63ae26f725f4b74dc4b7cd449af523
Jacopo Teneggi <jtenegg1@jh.edu>
2023-04-10 14:03:08 -0400

    First commit

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,16 @@ dependencies = ["torch"]
 
 import os
 import torch
+from demo.BBBC041.model import BBBC041TrophozoiteNet as _bbbc041trophozoitenet
 from demo.RSNA_ICH_detection.model import RSNAHemorrhageNet as _rsnahemorrhagenet
+
+
+def bbbc041trophozoitenet():
+    model = _bbbc041trophozoitenet()
+    dirname = os.path.dirname(__file__)
+    state_dict = torch.load(os.path.join(dirname, "demo", "BBBC041", "model.pt"))
+    model.load_state_dict(state_dict)
+    return model
 
 
 def rsnahemorrhagenet():
